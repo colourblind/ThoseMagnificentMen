18b4f214e9ec67ca0479ed71ffe568a105de7ead
colourblind <tom.milsom@gmail.com>
2010-11-18 13:26:08 +0000

    Tweaked speeds

--- src/Constants.h
+++ src/Constants.h
@@ -2,11 +2,11 @@
 #define THOSEMAGNIFICENTMEN_CONSTANTS_H
 
 const float GRAVITY = 0.0003f;
-const float STALL_SPEED = 0.01f;
-const float MAX_SPEED = 0.3f;
+const float STALL_SPEED = 0.05f;
+const float MAX_SPEED = 0.35f;
 const float START_SPEED = 0.15f;
 const float INVINCIBILITY_TIME = 3000;
-const float EXPLOSION_LENGTH = 5000;
+const float EXPLOSION_LENGTH = 4000;
 const float MAX_EXPLOSION_SPEED = 0.2f;
 const float BULLET_SPEED = 0.6f;
 const int NUM_SHRAPNEL = 20;
